18444db46fdb8352dc07c63ef0d4eac30a24b838
helminster32 <david.santos.gonzalez@alumnos.upm.es>
2017-05-17 21:41:49 +0200

    Preguntas tema 7

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
@@ -11,9 +13,18 @@ router.get('/author', function(req, res, next) {
     res.render('author');
 });
 
-// Pagina de ayuda
-router.get('/help', function(req, res, next) {
-    res.render('help');
-});
+
+// Definición de rutas de /quizzes
+router.get('/quizzes',                     quizController.index);
+router.get('/quizzes/:quizId(\\d+)',       quizController.show);
+router.get('/quizzes/new',                 quizController.new);
+router.post('/quizzes',                    quizController.create);
+router.get('/quizzes/:quizId(\\d+)/edit',  quizController.edit);
+router.put('/quizzes/:quizId(\\d+)',       quizController.update);
+router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
+
+router.get('/quizzes/:quizId(\\d+)/play',  quizController.play);
+router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
+
 
 module.exports = router;
