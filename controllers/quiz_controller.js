5294f52e0f8faf7ebe53a6a54f169535135f866e
helminster32 <david.santos.gonzalez@alumnos.upm.es>
2017-05-18 11:52:30 +0200

    Quiz tema 9

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,19 +1,42 @@
 var models = require("../models");
+var Sequelize = require('sequelize');
+
+
+// Autoload el quiz asociado a :quizId
+exports.load = function (req, res, next, quizId) {
+
+    models.Quiz.findById(quizId)
+    .then(function (quiz) {
+        if (quiz) {
+            req.quiz = quiz;
+            next();
+        } else {
+            throw new Error('No existe ningún quiz con id=' + quizId);
+        }
+    })
+    .catch(function (error) {
+        next(error);
+    });
+};
 
 
 // GET /quizzes
 exports.index = function (req, res, next) {
 
-    var quizzes = models.Quiz.findAll();
-
-    res.render('quizzes/index.ejs', {quizzes: quizzes});
+    models.Quiz.findAll()
+    .then(function (quizzes) {
+        res.render('quizzes/index.ejs', {quizzes: quizzes});
+    })
+    .catch(function (error) {
+        next(error);
+    });
 };
 
 
 // GET /quizzes/:quizId
 exports.show = function (req, res, next) {
 
-   res.render('quizzes/show', {quiz: req.quiz});
+    res.render('quizzes/show', {quiz: req.quiz});
 };
 
 
@@ -25,64 +48,91 @@ exports.new = function (req, res, next) {
     res.render('quizzes/new', {quiz: quiz});
 };
 
+
 // POST /quizzes/create
 exports.create = function (req, res, next) {
 
-    var quiz = {
+    var quiz = models.Quiz.build({
         question: req.body.question,
         answer: req.body.answer
-    };
+    });
 
-    // Validar que no estan vacios
-    if (!quiz.question || !quiz.answer) {
+    // guarda en DB los campos pregunta y respuesta de quiz
+    quiz.save({fields: ["question", "answer"]})
+    .then(function (quiz) {
+        res.redirect('/quizzes/' + quiz.id);
+    })
+    .catch(Sequelize.ValidationError, function (error) {
+
+        console.log('Errores en el formulario:');
+        for (var i in error.errors) {
+            console.log(error.errors[i].value);
+        }
+
         res.render('quizzes/new', {quiz: quiz});
-        return;
-    }
-
-    // guarda el nuevo quiz
-    quiz = models.Quiz.create(quiz);
-
-    res.redirect('/quizzes/' + quiz.id);
+    })
+    .catch(function (error) {
+        next(error);
+    });
 };
 
 
 // GET /quizzes/:quizId/edit
 exports.edit = function (req, res, next) {
 
-	res.render('quizzes/edit', {quiz: req.quiz});
+    res.render('quizzes/edit', {quiz: req.quiz});
 };
 
 
 // PUT /quizzes/:quizId
 exports.update = function (req, res, next) {
 
-        req.quiz.question = req.body.question;
-        req.quiz.answer = req.body.answer;
+    req.quiz.question = req.body.question;
+    req.quiz.answer = req.body.answer;
 
-        models.Quiz.update(req.quiz);
-
+    req.quiz.save({fields: ["question", "answer"]})
+    .then(function (quiz) {
         res.redirect('/quizzes/' + req.quiz.id);
+    })
+    .catch(Sequelize.ValidationError, function (error) {
+
+        console.log('Errores en el formulario:');
+        for (var i in error.errors) {
+            console.log(error.errors[i].value);
+        }
+
+        res.render('quizzes/edit', {quiz: req.quiz});
+    })
+    .catch(function (error) {
+        next(error);
+    });
 };
 
 
 // DELETE /quizzes/:quizId
 exports.destroy = function (req, res, next) {
 
-        models.Quiz.destroy(req.quiz);
-
+    req.quiz.destroy()
+    .then(function () {
         res.redirect('/quizzes');
+    })
+    .catch(function (error) {
+        next(error);
+    });
 };
+
 
 // GET /quizzes/:quizId/play
 exports.play = function (req, res, next) {
 
     var answer = req.query.answer || '';
 
-        res.render('quizzes/play', {
-            quiz: req.quiz,
-            answer: answer
-        });
+    res.render('quizzes/play', {
+        quiz: req.quiz,
+        answer: answer
+    });
 };
+
 
 // GET /quizzes/:quizId/check
 exports.check = function (req, res, next) {
@@ -91,22 +141,9 @@ exports.check = function (req, res, next) {
 
     var result = answer.toLowerCase().trim() === req.quiz.answer.toLowerCase().trim();
 
-        res.render('quizzes/result', {
-            quiz: req.quiz,
-            result: result,
-            answer: answer
-        });
+    res.render('quizzes/result', {
+        quiz: req.quiz,
+        result: result,
+        answer: answer
+    });
 };
-
-// Autoload el quiz asociado a :quizId
-exports.load = function (req, res, next, quizId) {
-
-	var quiz = models.Quiz.findById(Number(quizId));
-
-	if (quiz) {
-		req.quiz = quiz;
-		next();
-	} else {
-		throw new Error('No existe ningún quiz con id=' + quizId);
-		}
-	};
