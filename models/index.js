915f3d4c1556cd5b681db4d15bab07275c4fcd11
helminster32 <david.santos.gonzalez@alumnos.upm.es>
2017-05-20 12:00:18 +0200

    quiz Tema 13

--- models/index.js
+++ models/index.js
@@ -3,22 +3,28 @@ var path = require('path');
 // Cargar ORM
 var Sequelize = require('sequelize');
 
-// Usando BBDD SQLite:
-var sequelize = new Sequelize("sqlite:///", {storage: "quiz.sqlite"});
+// Para usar en local BBDD SQLite:
+//    DATABASE_URL = sqlite:///
+//    DATABASE_STORAGE = quiz.sqlite
+// Para usar en Heroku BBDD Postgres:
+//    DATABASE_URL = postgres://user:passwd@host:port/database
+
+var url, storage;
+
+if (!process.env.DATABASE_URL) {
+    url = "sqlite:///";
+    storage = "quiz.sqlite";
+} else {
+    url = process.env.DATABASE_URL;
+    storage = process.env.DATABASE_STORAGE || "";
+}
+
+var sequelize = new Sequelize(url, {storage: storage});
+
+
 
 // Importar la definicion de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
-
-
-// Crear tablas
-sequelize.sync()
-.then(function () {
-    console.log('Bases de datos creadas con éxito');
-})
-.catch(function (error) {
-    console.log("Error creando las tablas de la BBDD:", error);
-    process.exit(1);
-});
 
 
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
